af54404ab23ccd01d08e44f82774bd4bfaffce6c
Alyssa Aiello <alyssaaiello4@gmail.com>
2024-09-15 19:30:42 -0500

    add basic api functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react';
+import { login, logout, getBreeds, searchDogs, getDogs } from './common/api';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [breeds, setBreeds] = useState();
+
+    useEffect(() => {
+        getBreeds().then(res => setBreeds(res));
+        searchDogs().then(res => {
+            console.log(res);
+            getDogs(res.resultIds).then(res => {
+                console.log(res);
+            })
+        });
+    }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Finleys Friends</p>
+      <button onClick={login}>
+        Login
+      </button>
+
+      <button onClick={logout}>
+        Logout
+      </button>
+
+      
+
+      {(breeds && breeds.length > 0) &&
+      <>
+        <ul>
+            { breeds.map((breed, i) => (
+                <li key={i}>{breed}</li>
+            ))}
+        </ul> 
+
+        <Autocomplete
+            multiple
+            limitTags={2}
+            id="multiple-limit-tags"
+            options={breeds}
+            getOptionLabel={(option) => option}
+            defaultValue={[breeds[13], breeds[12], breeds[11]]}
+            renderInput={(params) => (
+                <TextField {...params} label="limitTags" placeholder="Favorites" />
+            )}
+            sx={{ width: '500px' }}
+        />
+        </>
+      }
+
+    {/* {(dogs && dogs.length > 0 && searchResults && searchResults.length > 0) &&
+        <ul>
+            { dogs.map(dog  => (
+                <p key={dog.id}>{dog.name}</p>
+            ))}
+        </ul> 
+      } */}
     </>
   )
 }
---
+++ src/common/api.js
@@ -0,0 +1,75 @@
+const baseUrl = "https://frontend-take-home-service.fetch.com";
+
+export async function login() {
+    const loginRes = await fetch(`${baseUrl}/auth/login`, {
+        credentials: "include",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: "User",
+            email: "user@email.com"
+        })
+    });
+
+    if (!loginRes.ok) {
+        throw new Error(`An error has occured: ${loginRes.status}`);
+    } else {
+        console.log("authenticated!");
+    }
+}
+
+export async function logout() {
+    const logoutRes = await fetch(`${baseUrl}/auth/logout`, {
+        credentials: "include",
+        method: "POST"
+    });
+
+    if (!logoutRes.ok) {
+        throw new Error(`An error has occured: ${logoutRes.status}`);
+    } else {
+        console.log("session ended")
+    }
+}
+
+export async function getBreeds() {
+    const breedsRes = await fetch(`${baseUrl}/dogs/breeds`, {
+        credentials: "include"
+    });
+
+    if (!breedsRes.ok) {
+        throw new Error(`An error has occured: ${breedsRes.status}`);
+    }
+
+    return await breedsRes.json();
+}
+
+export async function searchDogs() {
+    const searchRes = await fetch(`${baseUrl}/dogs/search`, {
+        credentials: "include"
+    });
+
+    if (!searchRes.ok) {
+        throw new Error(`An error has occured: ${searchRes.status}`);
+    }
+
+    return await searchRes.json();
+}
+
+export async function getDogs(dogIds) {
+    const dogsRes = await fetch(`${baseUrl}/dogs`, {
+        credentials: "include",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dogIds)
+    });
+
+    if (!dogsRes.ok) {
+        throw new Error(`An error has occured: ${dogsRes.status}`);
+    }
+
+    return await dogsRes.json();
+}
